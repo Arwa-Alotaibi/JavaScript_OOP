a652cbca6cb0e9a16d227e85c3785786f5ec6cb0
Arwa-Alotaibi <arwa.alosaimi@outlook.sa>
2022-08-15 13:24:38 +0300

    super_ninja

---
+++ Super_Ninja/Ninja.js
@@ -0,0 +1,47 @@
+class Ninja{
+    //constructor
+    constructor(name,health,speed ,strength){
+        this.name = name ; 
+
+        this.health= health ; 
+
+       // this.speed = 3;
+       this.speed = speed
+
+       // this.strength = 3;
+
+        this.strength = strength ;
+
+    }
+    
+
+    sayName(){
+        console.log('the name is:'+this.name);
+    }
+
+    showStats(){
+        console.log('Name is : '+this.name +' strength: '+ this.strength +' speed: '+ this.speed +' health: '+ this.health);
+    }
+
+    drinkSake(){
+        this.health +=10;
+    }
+
+
+
+}
+class Sensei extends Ninja{
+    //constructor
+    constructor(name,wisdom){
+     super(name,200,10 ,10);
+     this.wisdom =10 ;
+    }
+    speakWisdom(){
+     super.drinkSake();
+    console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
